40a9f556763433d5a9795a4652ba50ef715e973b
YanickZengaffinen <42324758+YanickZengaffinen@users.noreply.github.com>
2018-09-29 16:16:57 +0200

    Add project files.

---
+++ AI/StateMachine/API/IState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI.StateMachine.API
+{
+    /*
+     * Class that represents a behaviour
+     */
+    interface IState
+    {
+        /// <summary>
+        /// Called whenenver the IState is being activated
+        /// </summary>
+        void OnActivate();
+
+        /// <summary>
+        /// Called whenever the IState is being deactivated
+        /// </summary>
+        void OnDeactivate();
+
+        /// <summary>
+        /// Called whenever this IState is active and being updated
+        /// </summary>
+        /// <param name="deltaTime"> The time that has passed since the last update </param>
+        void OnUpdate(double deltaTime);
+        
+        /// <returns> Is this IState currently considering itself active? </returns>
+        bool IsActive();
+
+    }
+}
---
+++ AI/StateMachine/API/IStateMachine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI.StateMachine.API
+{
+    /*
+     * This class contains all the states and their connections
+     */
+    internal interface IStateMachine
+    {
+        /// <returns> A list of all states the machine has </returns>
+        IList<IState> GetStates();
+
+        /// <summary>
+        /// Goes through all the states of the machine and updates them.
+        /// </summary>
+        /// <param name="timeDelta"> The time that has passed since the last update </param>
+        void Update(double deltaTime);
+
+    }
+}
