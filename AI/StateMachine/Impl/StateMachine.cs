41c8b8a512b0b24d166740ec344714f47300a191
YanickZengaffinen <42324758+YanickZengaffinen@users.noreply.github.com>
2018-09-29 17:13:08 +0200

    StateMachine Impl

--- AI/StateMachine/Impl/StateMachine.cs
+++ AI/StateMachine/Impl/StateMachine.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using AI.StateMachine.API;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AI.StateMachine.Impl
 {
-    internal class StateMachine
+    internal class StateMachine : IStateMachine
     {
+        private IList<IState> states;
 
 
+        public StateMachine()
+        {
+
+        }
+
+        public IList<IState> GetStates()
+        {
+            return states;
+        }
+
+        public void Update(double deltaTime)
+        {
+
+        }
     }
 }
